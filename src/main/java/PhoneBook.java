3f12d889f8e751eb10055297db7e8d60453b5427
SebastianPereiro <kuzmin_konstantin@bk.ru>
2023-04-12 23:18:26 +0300

    Stub and tests for findByNumber

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,19 +1,25 @@
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public class PhoneBook {
 
 
     private Map<String, String> map = null;
 
-    public int add(String name, String phoneNumber){
-        if(map == null){
+    public int add(String name, String phoneNumber) {
+        if (map == null) {
             map = new HashMap<>();
             map.put(name, phoneNumber);
         }
 
         map.put(name, phoneNumber);
         return map.size();
+    }
+
+    public String findByNumber(String value) {
+
+        return null;
     }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -5,20 +5,34 @@ import org.junit.jupiter.api.Test;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class PhoneBookTest {
 
     PhoneBook phoneBook = new PhoneBook();
 
-    private Map<String, String> map;
+
 
     @Test
     void testAdd() {
-        map = new HashMap<>();
+
 
         int result = phoneBook.add("Братан", "8-999-134-22-11");
 
         int expected = 1;
+
+        assertEquals(expected, result);
+    }
+
+
+    @Test
+    void testFindByNumber(){
+        phoneBook.add("Братан", "8-999-134-22-11");
+
+        String result = phoneBook.findByNumber("8-999-134-22-11");
+
+        String expected = "Братан";
 
         assertEquals(expected, result);
     }
