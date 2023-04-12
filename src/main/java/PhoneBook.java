5e649b5e8cb163c71f084313a0d1e882cab1f460
SebastianPereiro <kuzmin_konstantin@bk.ru>
2023-04-12 22:07:42 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,14 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PhoneBook {
+
+
+    private Map<String, String> map = null;
+
+    public int add(String name, String phoneNumber){
+
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,25 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PhoneBookTest {
+
+    PhoneBook phoneBook = new PhoneBook();
+
+    private Map<String, String> map;
+
+    @Test
+    void testAdd() {
+        map = new HashMap<>();
+
+        int result = phoneBook.add("Братан", "8-999-134-22-11");
+
+        int expected = 1;
+
+        assertEquals(expected, result);
+    }
 }
